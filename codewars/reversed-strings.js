1066477a71c4ad04d408429f7656cdb2baf87c77
Kyle Canamar <canamar.kyle@gmail.com>
2022-09-12 17:37:35 -0700

    completes reversed strings

---
+++ codewars/reversed-strings.js
@@ -0,0 +1,13 @@
+/////////////////////
+// Question Reversed Strings 8kyu
+/////////////////////
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+/////////////////////
+// Solution 
+/////////////////////
+function solution(str){
+    return str.split("").reverse().join("")
+}
