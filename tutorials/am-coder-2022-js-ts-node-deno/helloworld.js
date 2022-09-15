e10fdb385cb81b491157b4749eb9e71bbadb073b
Kyle Canamar <canamar.kyle@gmail.com>
2022-09-14 19:06:13 -0700

    2/10 videos complete

---
+++ tutorials/am-coder-2022-js-ts-node-deno/helloworld.js
@@ -0,0 +1,12 @@
+// this will print text to the console
+console.log("hello world")
+
+// declare a variable with let
+let number = 4
+console.log(number)
+number = 5
+console.log(number)
+
+// declare a variable with const
+const bool = true 
+console.log(bool)
---
+++ tutorials/am-coder-2022-js-ts-node-deno/helloworld.ts
@@ -0,0 +1,12 @@
+// this will print to the console
+console.log("Hello World")
+
+// declare a variable with let
+let num:number = 5
+console.log(num)
+num = 6
+console.log(num)
+
+// declare variable with a const
+const myBoolean:boolean = true
+console.log(myBoolean)
