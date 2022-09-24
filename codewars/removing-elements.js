d09ec511cca796d9dd506b82a70caa6e3aba795e
Kyle Canamar <canamar@Kyles-MacBook-Pro-2.local>
2022-09-23 21:38:43 -0700

    completes removing elements

---
+++ codewars/removing-elements.js
@@ -0,0 +1,17 @@
+////////////////
+// Question Removing Elements 8kyu
+////////////////
+/*
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+////////////////
+// Solution
+////////////////
+function removeEveryOther(arr){
+    return arr.filter((element, index) => index % 2 === 0)
+  }
