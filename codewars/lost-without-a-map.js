ceaae6bdc5154db8d4f2143cb2acf8f5e498416f
Kyle Canamar <canamar.kyle@gmail.com>
2022-09-26 12:01:18 -0700

    completes lost without a map

---
+++ codewars/lost-without-a-map.js
@@ -0,0 +1,20 @@
+/////////////////////////
+// Question Lost without a Map 8kyu
+/////////////////////////
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+/////////////////////////
+// Solution 
+/////////////////////////
+function maps(x){
+    let double = []
+    for (let i = 0; i < x.length; i++){
+      double.push(x[i] * 2)
+    }
+    return double
+  }
