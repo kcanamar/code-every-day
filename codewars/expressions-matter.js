593c3d5eaede177c1aabd78e1f2a8485d892d2ff
Kyle Canamar <canamar.kyle@gmail.com>
2022-10-05 11:25:31 -0700

    completes expressions matter

---
+++ codewars/expressions-matter.js
@@ -0,0 +1,46 @@
+/////////////////////////
+// Qusetion Expressions Matter 8kyu
+/////////////////////////
+/*
+Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
+
+Notes
+The numbers are always positive.
+The numbers are in the range (1  ≤  a, b, c  ≤  10).
+You can use the same operation more than once.
+It's not necessary to place all the signs and brackets.
+Repetition in numbers may occur .
+You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+Input >> Output Examples:
+expressionsMatter(1,2,3)  ==>  return 9
+Explanation:
+After placing signs and brackets, the Maximum value obtained from the expression (1+2) * 3 = 9.
+*/
+/////////////////////////
+// Solution
+/////////////////////////
+
+function expressionMatter(a, b, c) {
+    let results = []
+    results.push(a * (b + c))
+    results.push(a * b * c)
+    results.push(a + b * c)
+    results.push((a + b) * c)
+    results.push(a + b + c)
+    // Math.max() returns the largest number within the argument passed 
+    return Math.max(...results)
+  }
+
+console.log(expressionMatter(2,3,5)) // 30
+console.log(expressionMatter(1,2,3)) // 9
+console.log(expressionMatter(14,3,7)) // 294
