d219aff731f1b3cea373e3f936cc1014dd4e6bea
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-27 18:12:36 -0800

    leetcode problems

---
+++ leetcode/add-nums.js
@@ -0,0 +1,8 @@
+// Question
+//////////////////////
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+*/
+// Solution
+//////////////////////
+var sum = (a, b) => a + b
---
+++ leetcode/root-equals.js
@@ -0,0 +1,15 @@
+// Question
+//////////////////////
+/*
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+Example 1:
+Input: root = [10,4,6]
+Output: true
+Explanation: The values of the root, its left child, and its right child are 10, 4, and 6, respectively.
+10 is equal to 4 + 6, so we return true.
+*/
+// Solution
+//////////////////////
