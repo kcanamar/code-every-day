dc7e10d9708a63fdaabdafcca641ad87f632c7c3
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-04-18 18:28:33 -0500

    other solution

--- codewars/swtich-it-up.js
+++ codewars/swtich-it-up.js
@@ -51,3 +51,19 @@ function switchItUp(number){
 //   ]
 //   return responses[number]
 // }
+
+function switchItUp(number) {
+  const responses = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+  }
+  return responses[number] ? responses[number] : 'Not a valid input'
+}
